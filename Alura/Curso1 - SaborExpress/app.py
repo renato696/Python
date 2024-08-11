f9e246bc808c534109a628ad09055ae45a2ab4f5
renato696 <renato696@gmail.com>
2024-08-11 10:19:05 -0300

    if - elif - else

--- Alura/Curso1 - SaborExpress/app.py
+++ Alura/Curso1 - SaborExpress/app.py
@@ -9,10 +9,22 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: ')) 
+#ja convertendo o input para integer
+# outra forma de converte a entrada do input de string para integer é converter a variável depois
+# opcao_escolhida = int(opcao_escolhida) 
+
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
 
