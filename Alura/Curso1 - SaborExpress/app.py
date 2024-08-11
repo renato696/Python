0516ea058b1f6077ded09d581d2b3590f97593d9
renato696 <renato696@gmail.com>
2024-08-11 10:54:33 -0300

    more defs

--- Alura/Curso1 - SaborExpress/app.py
+++ Alura/Curso1 - SaborExpress/app.py
@@ -11,29 +11,32 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░   
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: ')) 
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('clear')
     print('Finalizando o app\n')
 
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: ')) 
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes
 
 if __name__ == '__main__':
     main()
