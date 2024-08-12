3c0e165fb085857c6a2a68da930645d11f2cf1cd
renato696 <renato696@gmail.com>
2024-08-12 16:25:20 -0300

    try except 2

--- Alura/Curso1 - SaborExpress/app.py
+++ Alura/Curso1 - SaborExpress/app.py
@@ -26,25 +26,29 @@ def opcao_invalida():
     input('Digite uma tecla para voltar ao menu principal')
     main()
 
-def escolher_opcoes():
-    opcao_escolhida = int(input('Escolha uma opção: ')) 
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4:
-        finalizar_app
-    else:
-        opcao_invalida()
+def escolher_opcoes():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: ')) 
+            
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()        
 
 def main():
     os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
-    escolher_opcoes
+    escolher_opcoes()
 
 if __name__ == '__main__':
     main()
