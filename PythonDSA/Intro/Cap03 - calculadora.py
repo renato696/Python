9c2fd5f9c9891c87ba8cfd7d627cc6b2bc896ee3
renato696 <renato696@gmail.com>
2024-08-18 15:16:57 -0300

    Cap03 - calculadora pseudo

--- PythonDSA/Intro/Cap03 - calculadora.py
+++ PythonDSA/Intro/Cap03 - calculadora.py
@@ -1,4 +1,11 @@
 """
 Pseudocódigo:
-
+- mostrar o nome do programa "Calculadora Básica" com fonte diferenciada;
+- Solicitar o primeiro número a ser calculado e armazenar na memória;
+- Solicitar o segundo número a ser calculado e armazenar na memória;
+- Solcitar que o usuário informe a operação que deseja executar;
+- Comparar o operador escolhido com as opções e efetuar o cálculo.
+- Mostrar o resultado para o usuário;
+FIM
 """
+
