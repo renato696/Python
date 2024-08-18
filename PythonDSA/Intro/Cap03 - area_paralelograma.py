6a41f785d40f909e7f948e3fe48949e3512732c4
renato696 <renato696@gmail.com>
2024-08-18 13:55:23 -0300

    area do paralelograma

---
+++ PythonDSA/Intro/Cap03 - area_paralelograma.py
@@ -0,0 +1,11 @@
+"""
+Pseudocódigo:
+- Exiba 'Bem-vindo a Calculadora de Área do Paralelograma;
+- Peça para o usuário inserir o comprimento da base;
+- Armazene o comprimento da base em uma variável;
+- Peça para o usuário inserir a altura;
+- Armazene o resultado em uma variável;
+- Calcule a área do paralelograma: base * altura
+- Exiba o resultado
+"""
+
