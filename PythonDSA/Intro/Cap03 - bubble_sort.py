e7bd9cb7b0d5ada55b77cea3cd687572ca21d0b7
renato696 <renato696@gmail.com>
2024-08-19 14:23:22 -0300

    bubble_sort code

--- PythonDSA/Intro/Cap03 - bubble_sort.py
+++ PythonDSA/Intro/Cap03 - bubble_sort.py
@@ -1,4 +1,29 @@
 """
 Pseudocódigo:
-- organizar os elementos no array, 10 numeros.
+INICIO
+
+- para cada elemento i no array de tamanho n
+    - para cada elemento j no array de tamanho n - 1
+        - se elemento i for maior que elemento j
+            - troque os elementos i e j
+- Exiba o array ordenado 
+
+FIM
 """
+
+lista = [6, 7, 8, 3, 10, 19, 4, 1, 0, 61, 30, 16, 17, 82, 29, 34, 43, 21, 11, 39, 56, 67, 12]
+
+def bubble_sort(arr):
+
+    n = len(arr)
+
+    #para cada elemento i do array
+    for i in range(n):
+        #para cada elemento j do array
+        for j in range(0, n-i-1):
+            #se elemento i for maior que elemento j
+            if arr[j] > arr[j+1]:
+                #troque os elementos i e j
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+    
