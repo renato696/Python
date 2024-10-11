e976a30a8d2a6ba2c11fdc90a648032cc2710456
renato696 <renato696@gmail.com>
2024-10-11 10:47:16 -0300

    metodos proprios

--- Alura/POO/modelos/restaurante.py
+++ Alura/POO/modelos/restaurante.py
@@ -1,17 +1,22 @@
 class Restaurante:
+    restaurantes = []
+    
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False    
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | status: {restaurante.ativo}')
     
     
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Pizzaria')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restaurante.listar_restaurantes()
 
-print(restaurante_praca)
-print(restaurante_pizza)
