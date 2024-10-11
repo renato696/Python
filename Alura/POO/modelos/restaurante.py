89e286e21897a20e556567bdfb939b8d4c60be29
renato696 <renato696@gmail.com>
2024-10-11 10:37:16 -0300

    metodo string

--- Alura/POO/modelos/restaurante.py
+++ Alura/POO/modelos/restaurante.py
@@ -1,13 +1,17 @@
 class Restaurante:
     def __init__(self, nome, categoria):
-        nome = ''
-        categoria = ''
-        ativo = False
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False    
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
     
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+    
+restaurante_praca = Restaurante('Praca', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Pizzaria')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(dir(restaurante_praca))
-    
+print(restaurante_praca)
+print(restaurante_pizza)
