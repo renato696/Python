c5107f33919e2f72b57b1df54d022a10f676d75c
renato696 <renato696@gmail.com>
2024-10-14 11:55:03 -0300

    @classmethod

--- Alura/POO/modelos/restaurante.py
+++ Alura/POO/modelos/restaurante.py
@@ -2,26 +2,32 @@ class Restaurante:
     restaurantes = []
     
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self._ativo = True
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     @property
     def ativo(self):
         return '👍' if self._ativo else '👎'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | status: {restaurante.ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | status: {restaurante.ativo}')
+            
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+        
     
     
-restaurante_praca = Restaurante('Praca', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Pizzaria')
+restaurante_praca = Restaurante('praca', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Pizzaria')
 
 Restaurante.listar_restaurantes()
 
