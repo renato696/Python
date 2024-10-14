e8782633c89c260929d8b1c7bc49adda438ad758
renato696 <renato696@gmail.com>
2024-10-14 11:03:49 -0300

    POO - property e private

--- Alura/POO/modelos/restaurante.py
+++ Alura/POO/modelos/restaurante.py
@@ -4,15 +4,20 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = True
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
+    @property
+    def ativo(self):
+        return '👍' if self._ativo else '👎'
+    
     def listar_restaurantes():
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | status: {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | status: {restaurante.ativo}')
     
     
 restaurante_praca = Restaurante('Praca', 'Gourmet')
