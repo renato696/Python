c83706039c5833e212a7df18f196ef8347ae304d
renato696 <renato696@gmail.com>
2024-10-16 10:58:51 -0300

    desafio e final do curso

--- Alura/POO/app.py
+++ Alura/POO/app.py
@@ -1,9 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('LaPlaza', 'Gourmet')
-restaurante_praca.receber_avaliacao('Renato', 8)
-restaurante_praca.receber_avaliacao('Kosuke', 10)
-restaurante_praca.receber_avaliacao('Amora', 7)
+restaurante_praca.receber_avaliacao('Renato', 1)
+restaurante_praca.receber_avaliacao('Kosuke', 2)
+restaurante_praca.receber_avaliacao('Amora', 6)
 #restaurante_pizza = Restaurante('DonPizza', 'Pizzaria')
 #restaurante_hamburger = Restaurante('MrBurquer', 'Hamburgueria')
 
