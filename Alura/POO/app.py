5b84fac4ff67a4cba4da499e86646b253b1d0931
renato696 <renato696@gmail.com>
2024-10-18 11:45:00 -0300

    incluindo atributos as classes

--- Alura/POO/app.py
+++ Alura/POO/app.py
@@ -1,16 +1,24 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('LaPlaza', 'Gourmet')
-restaurante_praca.receber_avaliacao('Renato', 1)
-restaurante_praca.receber_avaliacao('Kosuke', 2)
-restaurante_praca.receber_avaliacao('Amora', 6)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+prato_omelete = Prato('Omelete', 20.00, 'Ovos mexidos com tomate e oregano')
+
+
+#restaurante_praca.receber_avaliacao('Renato', 1)
+#restaurante_praca.receber_avaliacao('Kosuke', 2)
+#restaurante_praca.receber_avaliacao('Amora', 6)
 #restaurante_pizza = Restaurante('DonPizza', 'Pizzaria')
 #restaurante_hamburger = Restaurante('MrBurquer', 'Hamburgueria')
 
 #restaurante_hamburger.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_omelete)
+    #Restaurante.listar_restaurantes()
 
 if __name__ == '__main__':
     main()
