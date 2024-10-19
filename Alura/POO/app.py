27a95a793a60bb76979963a94560e00c2f6f063f
renato696 <renato696@gmail.com>
2024-10-19 12:55:26 -0300

    polimorfismo

--- Alura/POO/app.py
+++ Alura/POO/app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('LaPlaza', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+bebida_suco.aplica_desconto()
 prato_omelete = Prato('Omelete', 20.00, 'Ovos mexidos com tomate')
+prato_omelete.aplica_desconto()
 restaurante_praca.adicionar_ao_cardapio(bebida_suco)
 restaurante_praca.adicionar_ao_cardapio(prato_omelete)
 
